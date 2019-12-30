4027e37663b77d5d865ec977ababea96c6247d20
MrDanikus <ddan62626@gmail.com>
2019-12-30 04:05:52 +0300

    Beta

---
+++ client.js
@@ -0,0 +1,90 @@
+const SERVER_NAME = "http://192.168.100.16:3000";
+
+const Colors = ["rgb(0,255,0)"];
+const Delay = 20;
+
+
+var elem = document.getElementById("col"); 
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) {
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { 
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { 
+    elem.msRequestFullscreen();
+  }
+}
+
+
+
+
+var sum = 255+255+255 + 1;
+
+var ClientServerOffset = 0.0;
+var ServerTime = 0.0;
+var offsets = [];
+const offs_size = 100;
+
+var flag = false;
+
+const socket = io(SERVER_NAME);
+
+function setColor(color){
+	elem.style.backgroundColor = color;
+}
+
+function syncClocks(){
+	socket.emit('Ping',pongCallback);
+	const clientPing = performance.now();
+	function pongCallback(serverPong){
+		const clientPong = performance.now();
+		const oneWayPing = (clientPong + clientPing)/2.;
+		var estimatedOffset = serverPong - oneWayPing;
+		offsets.unshift(estimatedOffset);
+		offsets.splice(offs_size);
+		let offsetsSum = 0.;
+		offsets.forEach((off) => {
+			offsetsSum += off;
+		});
+
+		ClientServerOffset = offsetsSum/offsets.length;
+		ServerTime = performance.now() + ClientServerOffset;
+
+		if(!flag && offsets.length == offs_size){
+			flag = true;
+			clearInterval(interval);
+			//interval = setInterval(syncClocks,100);
+			startLights();
+		}
+	}
+
+}
+
+
+
+
+var interval = setInterval(syncClocks,10);
+
+
+
+function startLights(){
+	setInterval(()=>{
+		var col = Math.floor((Math.floor(performance.now() + ClientServerOffset)  % (Delay*sum))/Delay);
+
+		if(col <= 255){
+			setColor(`rgb(${col},0,${255 - col})`);
+		}else if(col <= 255 + 255){
+			setColor(`rgb(${255 + 255 - col},${col-255},0)`);
+		}else{
+			setColor(`rgb(0,${255 + 255 + 255 - col},${col - 255 - 255})`);
+		}
+		
+	},10);
+}
+
+
+
+
---
+++ server.js
@@ -0,0 +1,91 @@
+//Before executing
+require('dotenv').config();
+
+var fs              = require('fs');
+var express         = require('express');
+var cors            = require('cors');
+var app             = express();
+var server          = require('http').Server(app);
+var io              = require('socket.io')(server);
+
+
+
+server.listen(process.env.PORT || 3000, function(){
+    console.log(`server is running on port ${process.env.PORT || 3000}`);
+})
+
+
+var path                            = require('path');
+
+const bodyParser                    = require('body-parser');
+var jsonParser                      = bodyParser.json();
+
+const { PerformanceObserver, performance } = require('perf_hooks');
+
+
+
+//WebSocket
+
+
+
+
+
+io.on('connection',function(socket){
+
+
+
+  socket.on('message',function(message){
+    let date = JSON.parse(message);
+
+
+  })
+
+  socket.on('Ping',function(pongCallback){
+    let serverTime = performance.now();
+    pongCallback(serverTime);
+  })
+
+})
+
+
+
+
+// Middleware
+
+
+
+
+
+function registration(req,res,next){
+
+}
+
+
+
+
+app.get('',jsonParser,function(req,res){
+  res.sendFile(__dirname + '/index.html');
+ 
+})
+
+
+app.get('*/client.js',jsonParser,function(req,res,next){
+  res.type('.js');
+  res.sendFile(__dirname + '/client.js');
+})
+
+
+app.get('*/socket/*',jsonParser,function(req,res,next){
+  res.type('.js');
+  res.sendFile(__dirname + `/socket/${req.originalUrl.split('/socket/')[1]}`);
+})
+
+
+
+
+
+
+
+
+
+
